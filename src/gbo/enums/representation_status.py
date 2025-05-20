eddd8e688dfa354a12ebac4c0118856a1318882f
thdfw <th.defauw@gmail.com>
2025-05-20 11:39:56 +0200

    Adding represenation status to the table by moving status to the color of the house alias badge

---
+++ src/gbo/enums/representation_status.py
@@ -0,0 +1,29 @@
+# Literal Enum:
+#  - no additional values can be added over time.
+#  - Sent as-is, not in hex symbol
+from enum import auto
+from typing import List
+
+from gw.enums import GwStrEnum
+
+
+class RepresentationStatus(GwStrEnum):
+
+    Unknown = auto()
+    ListeningToAtn = auto()
+    NotListeningToAtn = auto()
+
+    @classmethod
+    def values(cls) -> List[str]:
+        """
+        Returns enum choices
+        """
+        return [elt.value for elt in cls]
+
+    @classmethod
+    def default(cls) -> "RepresentationStatus":
+        return cls.Unknown
+
+    @classmethod
+    def enum_name(cls) -> str:
+        return "representation.status"
