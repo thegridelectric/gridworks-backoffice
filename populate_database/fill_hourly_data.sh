2e1574023541c89f3a47c391aad7bbb6daa8fc32
thdfw <th.defauw@gmail.com>
2025-11-14 16:52:29 +0100

    Debug cron job

--- populate_database/fill_hourly_data.sh
+++ populate_database/fill_hourly_data.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/ubuntu/gridworks-backoffice/populate_database
+cd /home/ubuntu/gridworks-backoffice
 source .venv/bin/activate
 python add_hourly_data.py >> /home/ubuntu/gridworks-backoffice/populate_database/cron.log 2>&1
 
