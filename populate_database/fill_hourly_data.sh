e9b3d57ded1fe67a551ba93a3d7551c484952dab
thdfw <th.defauw@gmail.com>
2026-01-08 11:25:53 +0100

    Update cron job

--- populate_database/fill_hourly_data.sh
+++ populate_database/fill_hourly_data.sh
@@ -3,5 +3,5 @@ cd /home/ubuntu/gridworks-backoffice
 source .venv/bin/activate
 cd populate_database
 python add_hourly_data.py >> /home/ubuntu/gridworks-backoffice/populate_database/cron.log 2>&1
-python populate_database/add_house_params.py >> /home/ubuntu/gridworks-backoffice/populate_database/cron.log 2>&1
+python add_house_params.py >> /home/ubuntu/gridworks-backoffice/populate_database/cron.log 2>&1
 
